d17c71beb906de8ffb83c474ee4e71e2b91be43c
mishort <mishort@bu.edu>
2022-10-25 11:30:34 -0400

    remove template content

--- run.py
+++ run.py
@@ -139,38 +139,5 @@ workflow.add_task(
     name="Assigning taxonomy to queries"
     )
 
-
-# Task2 sample R module  - src/analysis_example.r
-#workflow.add_task(
-#    "src/analysis.R -o [targets[0]] -d "+args.metadata,     #Command 
-#    depends=[TrackedExecutable("src/analysis.R")],          #Tracking executable dependencies
-#    targets=args.output,                                    #Output target directory
-#    args=[args.metadata])                                   #Additional arguments 
-
-
-# Task3 add_task_group  - AnADAMA2 example to execute a task on multiple input files/dependencies
-#multiple_input_files = glob(os.path.join(args.output, '*.txt')) #Initializing multiple input files 
-#output_files = [os.path.join(args.output,'data',os.path.basename(files+"_backup")) for files in multiple_input_files]
-#workflow.add_task_group(
-#    "cp [depends[0]] [targets[0]]",                            #Command 
-#    depends=[multiple_input_files],   #Tracking executable dependencies
-#    targets=output_files)                                      #Output target directory
-
-
-# private python function definition 
-#def remove_end_tabs_function(task):
-#    with open(task.targets[0].name, 'w') as file_handle_out:
-#        for line in open(task.depends[0].name):
-#            file_handle_out.write(line.rstrip() + "\n")
-            
-            
-# Task4 add_task  - AnADAMA2 example to usage of python task function 
-#workflow.add_task(
-#    remove_end_tabs_function,                       #Calling the python function  
-#    depends=args.input,                             #Tracking executable dependencies
-#    targets=args.output+"/data/data.tsv.notabs",    #Target output
-#    name="remove_end_tabs")
-
-
 # Run the workflow
 workflow.go()
