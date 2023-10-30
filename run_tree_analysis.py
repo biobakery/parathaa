874ebcf6da077052032268716aed2e056f57fe87
nearinj <nearing@broadinstitute.org>
2023-10-30 14:45:58 -0400

    Fixed bug in SILVA name parameter

--- run_tree_analysis.py
+++ run_tree_analysis.py
@@ -128,7 +128,7 @@ workflow.add_task(
 
 # See R script for comments
 workflow.add_task(
-    "src/find.cutoffs_flex_parallel.R   -d [depends[1]] -o [args[0]] -n [depends[2]] --wt1 [args[1]] --wt2 [args[2]] --threads [args[3]] --name [args[[4]]] ",
+    "src/find.cutoffs_flex_parallel.R   -d [depends[1]] -o [args[0]] -n [depends[2]] --wt1 [args[1]] --wt2 [args[2]] --threads [args[3]] --name [args[4]]",
     depends=[TrackedExecutable("src/analysis.R"), args.taxonomy, args.tree],
     targets= [args.output+"/optimal_scores.png"],
     args=[args.output, args.sweight, args.mweight, args.threads, args.namePar],
